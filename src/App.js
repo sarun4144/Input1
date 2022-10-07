48221d88e04ac1ea3961f8ac00415607dc20f117
sarun4144 <sarun4144@gmail.com>
2022-10-07 11:21:26 +0700

    sarun

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import FormValidation from './form-validation';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Web Development Quiz</h1>
-    </div>
+   
+      <FormValidation/>
+    
   );
 }
 
